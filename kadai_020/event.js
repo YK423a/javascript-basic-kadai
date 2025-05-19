cd55c76e5df40835f524168879f5e08b7ba5f829
YK423a <mwdjg0@gmail.com>
2025-05-19 23:16:39 +0900

    event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -3,5 +3,5 @@ const parentlist = document.getElementById('parent-list');
 btn.addEventListener('click', () => {
     const a = document.createElement('p');
     a.textContent = 'ボタンをクリックしました';
-    parentlist.appendChild(a);
+    text.appendChild(a);
 });
