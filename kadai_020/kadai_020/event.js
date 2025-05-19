d779a3c17741aab7fc1c4f89d808b4ee2305e7da
YK423a <mwdjg0@gmail.com>
2025-05-19 23:29:09 +0900

    Add files via upload

--- kadai_020/kadai_020/event.js
+++ kadai_020/kadai_020/event.js
@@ -1,7 +1,5 @@
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 btn.addEventListener('click', () => {
-    const a = document.createElement('h2');
-    a.textContent = 'ボタンをクリックしました';
-    parentlist.appendChild(a);
+    text.textContent = 'ボタンをクリックしました';
 });
