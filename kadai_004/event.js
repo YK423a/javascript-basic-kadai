b81e491503636a3b5da9f86dc4dceb4ec7ada072
YK423a <mwdjg0@gmail.com>
2025-05-21 21:43:56 +0900

    Add files via upload

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,7 +1,7 @@
-$(div).on('load',function(){
+$(window).on('load',function(){
  console.log('loadイベントが発生しました');
 });
 
-$(div).on('scroll',function(){
+$(window).on('scroll',function(){
  console.log('scrollイベントが発生しました');
 });
